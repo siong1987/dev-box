f0f1499014b6e5576f9525798e71b1853dcc3ff0
Teng Siong Ong <siong1987@gmail.com>
2013-11-25 15:47:47 -0800

    Add elastic search.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,11 +19,16 @@ sudo apt-get update
 sudo apt-get -y install nodejs
 
 # mongodb
-
 sudo apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get -y --force-yes install mongodb-10gen
+
+# elasticsearch
+sudo apt-get -y install openjdk-7-jre-headless
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.deb
+sudo dpkg -i elasticsearch-0.90.3.deb
+sudo service elasticsearch start
 
 # postgres
 sudo apt-get -y install postgresql
